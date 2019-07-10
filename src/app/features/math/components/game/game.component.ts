42fef84054119a5401d01b39e3e0554e31c7e2e8
Jeff Gonzalez <jeff@Hypertheory>
2019-07-10 15:37:38 -0400

    end of day 1

---
+++ src/app/features/math/components/game/game.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionModel } from '../../models';
+import { MathState, selectQuestionModel, selectAtEndOfQuestions } from '../../reducers';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { answerProvided } from '../../actions/questions.actions';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+
+  model$: Observable<QuestionModel>;
+  atEnd$: Observable<boolean>;
+  constructor(private store: Store<MathState>) { }
+
+  ngOnInit() {
+    this.model$ = this.store.select(selectQuestionModel);
+    this.atEnd$ = this.store.select(selectAtEndOfQuestions);
+  }
+
+  next(guessEl: HTMLInputElement) {
+    const guess = guessEl.valueAsNumber;
+    this.store.dispatch(answerProvided({ guess }));
+    guessEl.value = ''; // clear it out for the next question
+    guessEl.focus(); // put there cursor back there
+  }
+
+}
