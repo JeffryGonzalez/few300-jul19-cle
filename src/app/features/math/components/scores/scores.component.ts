664d1ace6f97b7470295ec3f7de97915b9b1ead8
Jeff Gonzalez <jeff@Hypertheory>
2019-07-11 15:15:29 -0400

    done with practice

--- src/app/features/math/components/scores/scores.component.ts
+++ src/app/features/math/components/scores/scores.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { ScoresModel } from '../../models';
 import { Store } from '@ngrx/store';
 import { MathState, selectScoresModel } from '../../reducers';
+import { Router } from '@angular/router';
+import { playAgain } from '../../actions/questions.actions';
 
 @Component({
   selector: 'app-scores',
@@ -11,10 +13,14 @@ import { MathState, selectScoresModel } from '../../reducers';
 })
 export class ScoresComponent implements OnInit {
   scoresModel$: Observable<ScoresModel>;
-  constructor(private store: Store<MathState>) { }
+  constructor(private store: Store<MathState>, private router: Router) { }
 
   ngOnInit() {
     this.scoresModel$ = this.store.select(selectScoresModel);
   }
 
+  playAgain() {
+    this.store.dispatch(playAgain());
+    this.router.navigate(['math', 'game']);
+  }
 }
